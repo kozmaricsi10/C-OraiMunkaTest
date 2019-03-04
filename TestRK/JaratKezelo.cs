a68db6dd19e0a3659ed3b856682c627faaedaf82
Petrik Lajos Szg <info@petrik.hu>
2019-03-04 16:14:55 +0100

    teszt

---
+++ TestRK/JaratKezelo.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRK
+{
+    public class JaratKezelo
+    {
+        List<Jarat> jaratok = new List<Jarat>();
+
+        public void UjJarat(string jaratSzam, string repterHonnan, string repterHova, DateTime indulas)
+        {
+            if (Kereses(jaratSzam) == null)
+            {
+                jaratok.Add(new Jarat(jaratSzam, repterHonnan, repterHova, indulas));
+            }
+        }
+
+        public DateTime MikorIndul(string jaratSzam)
+        {
+            return Kereses(jaratSzam).getIndulas();
+        }
+
+        public void Keses(string jaratSZam, int keses)
+        {
+            if (Kereses(jaratSZam).JaratSzam.Equals(jaratSZam))
+            {
+                Kereses(jaratSZam).setKeses(keses);
+            }
+        }
+
+        private Jarat Kereses(string jaratSzam)
+        {
+            foreach (var i in jaratok)
+            {
+                if (i.getJaratszam().Equals(jaratSzam))
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+
+        public List<string> JaratokRepuloterrol(string repter)
+        {
+            List<string> vissza = new List<string>();
+            foreach (var i in jaratok)
+            {
+                if (i.getHonnanRepter().Equals(repter))
+                {
+                    vissza.Add(i.getHonnanRepter());
+                }
+            }
+            return vissza;
+        }
+        
+
+        class Jarat
+        {
+            public string JaratSzam;
+            public string HonnanRepter;
+            public string HovaRepter;
+            public DateTime Indulas;
+            public int Keses;
+
+            public Jarat(string jaratSzam, string honnanRepter, string hovaRepter, DateTime indulas)
+            {
+                this.JaratSzam = jaratSzam;
+                this.HonnanRepter = honnanRepter;
+                this.HovaRepter = hovaRepter;
+                this.Indulas = indulas;
+                this.Keses = 0;
+            }
+
+            public string getJaratszam()
+            {
+                string vissza = this.JaratSzam;
+                return vissza;
+            }
+
+            public void setKeses(int keses)
+            {
+                if (this.Keses + keses < 0)
+                {
+                    throw new NegativKesesException("Negativ keses exception");
+                }
+                else
+                {
+                    this.Keses += keses;
+                }
+
+            }       
+
+            public string getHonnanRepter()
+            {
+                string vissza = this.HonnanRepter;
+
+                return vissza;
+            }
+
+            public DateTime getIndulas()
+            {
+                DateTime ido = this.Indulas;
+
+                return ido.AddMinutes(this.Keses);
+            }
+        }
+
+        [Serializable()]
+        class NegativKesesException : Exception
+        {
+            
+            public NegativKesesException(string message, System.Exception inner) : base(message, inner) { }
+
+            public NegativKesesException(string message) : base(message) { }
+
+            public NegativKesesException() : base() { }
+
+
+            protected NegativKesesException(System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext context) : base(info, context)
+            { }
+
+        }
+    }
+}
---
+++ TestRK/Test/Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestRK.Test
+{
+    class Test
+    {
+        JaratKezelo test;
+
+        [SetUp]
+        public void SetUp()
+        {
+            test = new JaratKezelo();
+        }
+
+        [Test]
+        public void UjJarat()
+        {
+            test.UjJarat("1548", "Budapest", "Cyprus", new DateTime(2019, 3, 30, 4, 05, 0));
+
+        }
+      
+        [Test]
+        public void Keses()
+        {
+
+            test.UjJarat("1548", "Budapest", "Cyprus", new DateTime(2019, 3, 30, 4, 05, 0));
+            test.Keses("1548", 30);
+            Assert.AreEqual(new DateTime(2019, 3, 30, 4, 35, 0) , test.MikorIndul("1548"));
+        }
+
+        [Test]
+        public void Keses2()
+        {
+
+            test.UjJarat("1548", "Budapest", "Cyprus", new DateTime(2019, 3, 30, 4, 05, 0));
+            test.Keses("1548", -30);
+            Assert.AreEqual(new DateTime(2019, 3, 30, 3, 35, 0), test.MikorIndul("1548"));
+        }
+
+        [Test]
+        public void Keses3()
+        {
+
+            test.UjJarat("1548", "Budapest", "Cyprus", new DateTime(2019, 3, 30, 4, 05, 0));
+            test.Keses("1548", 40);
+            test.Keses("1548", -10);
+            Assert.AreEqual(new DateTime(2019, 3, 30, 4, 35, 0), test.MikorIndul("1548"));
+        }
+
+
+    }
+}
